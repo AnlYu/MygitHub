93f1af77c3b6b656febaa453513b5d0d7d56049c
DDS <1981424795@qq.com>
2018-11-29 14:14:45 +0800

    Initial commit

---
+++ Source/MygitHub/MygitHubGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MygitHubGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGITHUB_API AMygitHubGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
